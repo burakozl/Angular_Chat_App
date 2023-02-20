668f4423faa051bd7ee0d678379b811ed971c642
burakozl <burakk.oozel@gmail.com>
2023-02-20 12:00:41 +0300

    updated

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+
+const port = process.env.PORT || 3000;
+const globalRoom = "Global Room";
+
+io.on('connection', (socket) => {
+
+    //console.log(`Guest${socket.id.substr(3, 2)} connected`);
+   // console.log(`${user} connected`);
+
+    socket.on('message', (message) => {
+      console.log(message);
+      //io.emit('message', `${message}`);
+      socket.to(globalRoom).emit('message', {user: socket.data.user,message});
+    });
+
+    socket.on('join', (userName, callback) => {
+      //console.log(user);
+      socket.data.user = userName;
+      socket.join(globalRoom)
+      socket.to(globalRoom).emit('userJoined',socket.data.user);
+      callback(true)
+    });
+
+    socket.on('disconnect', () => {
+      //console.log(`Guest-${socket.id.substr(3, 2)} disconnected`);
+      console.log(`${socket.data.user} disconnected`);
+      socket.to(globalRoom).emit('userleft',socket.data.user);
+    });
+  
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
