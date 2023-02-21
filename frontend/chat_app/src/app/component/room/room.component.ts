043911b270e4467a0d11bae1072d7957ed0f0cfc
burakozl <burakk.oozel@gmail.com>
2023-02-21 16:22:19 +0300

    updated room

--- frontend/chat_app/src/app/component/room/room.component.ts
+++ frontend/chat_app/src/app/component/room/room.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalStorageUtil } from 'src/app/local-storage.util';
 import { ChatService } from 'src/app/services/chat.service';
-import { LocalstrorageService } from 'src/app/services/localstrorage.service';
 
 @Component({
   selector: 'app-room',
@@ -11,39 +12,63 @@ import { LocalstrorageService } from 'src/app/services/localstrorage.service';
   standalone: true,
   imports:[CommonModule,FormsModule]
 })
-export class RoomComponent {
+export class RoomComponent implements AfterViewInit {
+
+  @ViewChild('scroll', { static: true }) scroll: any;
 
   newMessage!: string;
   messageList: any[] = [];
+  users: any;
   userName!:string;
-  constructor(private chatService: ChatService,private localStrorageService:LocalstrorageService){
-    this.userName = JSON.parse(this.localStrorageService.get("user") || "Guest");
+
+  constructor(private chatService: ChatService, private router:Router){
+    this.userName = LocalStorageUtil.get("user","");
   }
 
   ngOnInit(){
-    this.chatService.joinTheRoom(this.userName);
+    if(!this.userName) {
+      this.router.navigateByUrl('/join');
+      return;
+    }
+    if(!this.chatService.isJoined){
+      this.joinRoom();
+    }else{
+      this.users = this.chatService.initialUsers;
+    }
     this.chatService.initEvents();
     this.chatService.userLeft$.subscribe((user: string) => {
       this.messageList.push({user:'system', message: `${user} has left the room`});
+      this.users = this.users.filter((u:any) => u != user);
     });
     this.chatService.userJoined$.subscribe((user: string) => {
       this.messageList.push({user:'system', message: `${user} joined`});
+      this.users.push(user);
     });
     this.chatService.message$.subscribe((data: any) => {
         this.messageList.push(data);
-
     })
+  }
+
+  ngAfterViewInit(){
+    this.scroll.nativeElement.scrollTo(0, this.scroll.nativeElement.scrollHeight);
+  }
+
+
+  async joinRoom() {
+     this.users = await this.chatService.joinTheRoom(this.userName);
   }
 
   sendMessage() {
     if(this.newMessage != null){
       this.chatService.sendMessage(this.newMessage);
       this.messageList.push({user:this.userName,message: this.newMessage});
+      setTimeout(() => {
+        this.scroll.nativeElement.scrollTo(0, this.scroll.nativeElement.scrollHeight);
+      }, 0);
       this.newMessage = '';
     }else{
       alert('Enter the text area something...')
     }
-    console.log(this.messageList);
 
   }
 }
