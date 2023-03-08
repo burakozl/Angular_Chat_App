966d190e8977bca408dba8eaf7fe996e7f8ba6cf
burakozl <burakk.oozel@gmail.com>
2023-03-08 14:09:47 +0300

    added broadcastService and updated message tranfer

--- frontend/chat_app/src/app/component/room/room.component.ts
+++ frontend/chat_app/src/app/component/room/room.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, ViewChild, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { BROADCAST_SERVICE } from 'src/app/app.tokens';
 import { FilterPipe } from 'src/app/customPipe/filter.pipe';
 import { LocalStorageUtil } from 'src/app/local-storage.util';
+import { BroadcastService } from 'src/app/services/broadcast.service';
 import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
@@ -13,7 +16,7 @@ import { ChatService } from 'src/app/services/chat.service';
   standalone: true,
   imports: [CommonModule, FormsModule, FilterPipe]
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit, OnDestroy {
 
   @ViewChild('scroll', { static: true }) scroll: any;
 
@@ -26,55 +29,74 @@ export class RoomComponent {
   showScrollDownIcon: boolean = false;
   isNearBottom: boolean = false;
   scrollContainer: any;
+  subscription = new Subscription();
 
-  constructor(private chatService: ChatService, private router: Router) {
+  constructor(private chatService: ChatService, private router: Router, @Inject(BROADCAST_SERVICE) public broadCastService: BroadcastService) {
     this.userName = LocalStorageUtil.get("user", "");
+    this.subscription.add(this.broadCastService.mustConnectSocket$.subscribe(() => {
+      this.chatService.connectSocket();
+    }));
+   this.broadCastService.sendPing();
   }
 
   ngOnInit() {
+
     if (!this.userName) {
       this.router.navigateByUrl('/join');
       return;
     }
+
     if (!this.chatService.isJoined) {
       this.joinRoom();
     } else {
       this.users = this.chatService.initialUsers;
     }
+
     this.chatService.initEvents();
+
     this.chatService.userLeft$.subscribe((user: string) => {
-      if(!user) return;
-      this.messageList.push({ user: 'system', message: `${user} has left the room` });
+      if (!user) return;
+      this.broadCastService.pushChatMessage({ user: 'system', message: `${user} has left the room` });
       this.users = this.users.filter((u: any) => u != user);
+      this.broadCastService.pushUsers(this.users);
       this.scrollIconControl();
     });
+
     this.chatService.userJoined$.subscribe((user: string) => {
-      this.messageList.push({ user: 'system', message: `${user} joined` });
+      this.broadCastService.pushChatMessage({ user: 'system', message: `${user} joined` });
+      this.broadCastService.pushUsers(user);
+      this.users = this.users.filter((u: any) => u != user);
       this.users.push(user);
       this.scrollIconControl();
     });
+
     this.chatService.message$.subscribe((data: any) => {
-      this.messageList.push(data);
+      this.broadCastService.pushChatMessage(data.message);
       this.scrollIconControl();
     });
+
+    this.scrollContainer = this.scroll.nativeElement;
+    this.broadCastChannel();
+    this.broadCastService.channelList
+
+
   }
 
   async joinRoom() {
     this.users = await this.chatService.joinTheRoom(this.userName);
   }
 
+
   sendMessage() {
     if (this.newMessage == null || this.newMessage == '') {
       alert('Enter the text area something...')
       return;
     }
-    this.chatService.sendMessage(this.newMessage);
-    this.messageList.push({ user: this.userName, message: this.newMessage });
+
+    this.broadCastService.pushChatMessage({ user: this.userName, message: this.newMessage });
+     //this.chatService.sendMessage(this.newMessage);
+    // this.messageList.push({ user: this.userName, message: this.newMessage });
     this.newMessage = '';
-    this.scrollContainer = this.scroll.nativeElement;
-    if (!this.showScrollDownIcon) {
-      this.scrollToBottom();
-    };
   }
 
   scrollIconControl() {
@@ -115,5 +137,25 @@ export class RoomComponent {
     else {
       this.showScrollDownIcon = false;
     }
+  }
+
+  broadCastChannel() {
+    this.subscription.add(this.broadCastService.chatList$.subscribe(message => {
+      this.messageList.push(message);
+
+      if(message.user == this.userName){
+        this.chatService.sendMessage(message);
+        //console.log(message);
+      }
+
+      this.scrollContainer = this.scroll.nativeElement;
+      if (!this.showScrollDownIcon) {
+        this.scrollToBottom();
+      };
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
