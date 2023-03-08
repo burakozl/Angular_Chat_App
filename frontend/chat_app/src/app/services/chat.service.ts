fe44b73a31b55b43baf9b08a7ba0229db434743d
burakozl <burakk.oozel@gmail.com>
2023-03-08 14:08:05 +0300

    updated

--- frontend/chat_app/src/app/services/chat.service.ts
+++ frontend/chat_app/src/app/services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { SocketService } from './socket.service';
+import { Subject, Subscription } from 'rxjs';
 import { io } from "socket.io-client";
 
 
@@ -15,9 +16,15 @@ export class ChatService {
 
   constructor() { }
 
-  socket = io('http://localhost:3000');
+  socket = io('http://localhost:3000',{autoConnect:false});
 
+  connectSocket(){
+    if(!this.socket.connected){
+      this.socket.connect();
+    }
+  }
   public sendMessage(message: any) {
+    if(!this.socket.connected) return;
     this.socket.emit('message', message);
   }
 
@@ -45,5 +52,62 @@ export class ChatService {
       });
     })
   }
+  // conType?: "socket" | "broadcast";
+  // connection?: BroadcastService | SocketService;
+  // connectionSub?: Subscription;
+  // isJoined:boolean = false;
+  // initialUsers: any = [];
 
+  // public message$: Subject<string> = new Subject();
+  // public userLeft$: Subject<string> = new Subject();
+  // public userJoined$: Subject<string> = new Subject();
+
+  // constructor(private broadcastService: BroadcastService, private socketService: SocketService) { }
+
+  // async connect() {
+  //   this.conType = await this.broadcastService.connect();
+  //   if (this.conType == "broadcast") {
+  //     this.connection = this.broadcastService;
+  //   } else {
+  //     this.connection = this.socketService;
+  //   }
+  //   this.destroyEvents();
+  //   this.listenEvents();
+  // }
+
+  // public sendMessage(message: any) {
+  //   this.connection.emit('message', message);
+  // }
+
+  // public joinTheRoom(userName: string) {
+  //   return new Promise(resolve => {
+  //     this.connection.emit('join', userName);
+  //     this.isJoined = true;
+  //     this.initialUsers = [];
+
+  //     resolve([]);
+  //   })
+  // }
+
+  // listenEvents() {
+  //   this.connectionSub = this.connection.onEvent$.subscribe((res: any) => {
+  //     switch (res.event) {
+  //       case 'message':
+  //         this.message$.next(res.data);
+  //         break;
+  //       case 'userleft':
+  //         this.userLeft$.next(res.data);
+  //         break;
+  //       case 'userJoined':
+  //         this.userJoined$.next(res.data);
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   })
+  // }
+
+  // destroyEvents() {
+  //   if (this.connectionSub) this.connectionSub.unsubscribe();
+  // }
 }
